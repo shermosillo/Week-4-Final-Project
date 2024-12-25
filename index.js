ccb00eb19b61374fee78636f66f6c8f721f66666
Sam <shermosillo27@gmail.com>
2024-12-25 08:09:04 -0700

    retrieve recipes

--- index.js
+++ index.js
@@ -3,37 +3,26 @@
 const appId = "5c80967a"
 const appKey = "e40187c7a1d8778d47a827f645b52f73"
 const appType = "public"
-const appIngredient = localStorage.getItem("ingr")
 const recipeListEl = document.querySelector(".recipe-card")
 
-async function getRecipes(event) {
-    const ingr = event.target.value;
-    renderRecipes(ingr)
-}
 
- async function renderRecipes() {
-    const recipes =  await fetch(`https://api.edamam.com/api/recipes/v2?type=${appType}&app_id=${appId}&app_key=${appKey}&q=${appIngredient}`)
+ async function getRecipes(event) {
+    const searchRecipe = event.target.value;
+    const recipes =  await fetch(`https://api.edamam.com/api/recipes/v2?type=${appType}&app_id=${appId}&app_key=${appKey}&q=${searchRecipe}`)
     const recipesData = await recipes.json();
-    recipeListEl.innerHTML = recipesData.hits.map(hit => recipeHTML(hit.recipe)).join("");
-    console.log(recipesData)
+    recipeListEl.innerHTML = recipesData.hits.map((hit) => recipeHTML(hit.recipe)).join("");
 }
-
-renderRecipes();
-
-
 
 function recipeHTML(recipe) {
-    return `<div class="recipe-card">
+    return `
+    <div class="recipe-card__container"  >
         <div class="recipe-card__items">
             <h3>${recipe.label}</h3>
             <p><b>Calories:</b> ${recipe.calories.toFixed(2)}</p>
-            <p><b>Ingredients:</b></p>
-            <ul> 
-                ${recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
-            </ul>
-            <figure>
-                    <img src="${recipe.image}" alt="">
-                </figure>
+            <figure class="recipe-card__img--wrapper">
+                <img class="recipe-card__img" src="${recipe.image}" alt="">
+            </figure>
         </div>
-    </div>`;
+    </div>
+    `;
 }
